24e7535d27de6de7a31e03bd224843330c5daf38
Amy <amysaraimr@gmail.com>
2025-02-16 12:54:49 -0800

    Supabase configurado y se creó el archivo .env

--- alura-medical-services/config/supabase.js
+++ alura-medical-services/config/supabase.js
@@ -1,11 +1,10 @@
-const { createClient } = require('@supabase/supabase-js');
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 if (!supabaseUrl || !supabaseKey) {
-  console.error('Error: Falta SUPABASE_URL o SUPABASE_KEY en el archivo .env');
-  process.exit(1);
+  console.error('Error: Falta SUPABASE_URL o SUPABASE_KEY en el archivo .env')
 }
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-module.exports = supabase;
+export const supabase = createClient(supabaseUrl, supabaseKey)
